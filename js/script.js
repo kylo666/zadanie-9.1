cd9e1e2776a1468a57d9087d22fd4e173b50dfe7
kylo666 <krzysztofhernas@gmail.com>
2017-10-18 16:38:54 +0200

    poprawka 9.1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-﻿1. //alert("komunikat");
-2 /*cofirm("Czy chcesz opuścić strone?");
-3 prompt(" Jak masz na imie?");
+﻿var getTriangleArea=function(a, h){
+if (a <= 0 || h<=0) {
+	alert ("Nieprawidlowe dane");
+}
+else {
+return a*h/2;
+}
+
+}
+
+console.log(getTriangleArea(10, 6))
+<script>
+var triangle1Area = getTriangleArea(10, 15);
+    triangle1Area2 = getTriangleArea(12, 5);
+    triangle1Area3 = getTriangleArea(1, 11);
+</script>
